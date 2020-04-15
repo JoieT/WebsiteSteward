dbe2fbe5ca7e6411d7ee76768d018fe0165ef678
JoieT <joiethacker@gmail.com>
2020-04-15 21:50:28 +0000

    added file names to index.js

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ app.set('view engine', 'ejs');
 app.get("/", (req,res) => {
  res.render("home"); //<— Change to main html file
 });
+app.get("/support", function(req,res){
+    res.render("support");
+});
+app.get("/solutions", function(req,res){
+    res.render("solutions");
+});
+
+ app.get("/dashboard", function(req,res){
+    res.render("dashboard");
+});
+    app.get("/support", function(req,res){
+    res.render("support");
+});
+ app.get("/template", function(req,res){
+    res.render("template");
+});
 app.listen(port, () => {
  console.log("Server running on port 3000");
 });
